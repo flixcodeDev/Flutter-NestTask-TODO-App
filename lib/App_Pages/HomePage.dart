b86897b9fec66a9d5018dcc1804f0a86938a2a13
mdtaosifhossain3 <mdtaosifhossain3@gmail.com>
2025-01-28 18:13:35 +0600

    service folder

--- lib/App_Pages/HomePage.dart
+++ lib/App_Pages/HomePage.dart
@@ -9,7 +9,6 @@ import 'package:nesttask/CustomButton/NavigationDrawer.dart';
 
 import 'package:nesttask/CustomButton/TaskContainer.dart';
 
-
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
 
@@ -18,24 +17,20 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-
   Color bgcolor = Colors.white;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       backgroundColor: Colors.white,
       drawer: NavDrawerPage(),
       appBar: AppBar(
         backgroundColor: Colors.white,
         leading: IconButton(
-          onPressed: (){
-
-            },
-          icon :Icon(Icons.account_circle,
-          size: 30,)
-
-        ),
+            onPressed: () {},
+            icon: Icon(
+              Icons.account_circle,
+              size: 30,
+            )),
         actions: [
           IconButton(
               onPressed: () {},
@@ -47,98 +42,111 @@ class _HomepageState extends State<Homepage> {
       ),
       body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Stack(children: [
-              SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Hello, Sakib!",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.01,
-                    ),
-                    Text(
-                      "Have a nice day.",
-                      style: TextStyle(
-                          color: Colors.black.withOpacity(.5),
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.01,
-                    ),
-                    Row(
-                      children: [
-                        Customcontainer(title: "My Task",onTap: (){},),
-                        SizedBox(width: MediaQuery.of(context).size.width * 0.04),
-                        Customcontainer(title: "In-Progress",onTap: (){},),
-                        SizedBox(width: MediaQuery.of(context).size.width * 0.04),
-                        Customcontainer(title: "Completed",onTap: (){},),
-                      ],
-                    ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-                    Text("Progress",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600)),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.04),
-                    CustomTaskContainer(
-                      title: "Data Science Quiz",
-                      subtitle: "Date- 19Feb - 28Feb",
-                      icon: Icon(
-                        Icons.quiz,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.04),
-                    CustomTaskContainer(
-                      title: "Data Science Presentation",
-                      subtitle: "Date- 20Feb - 28Feb",
-                      icon: Icon(
-                        Icons.present_to_all,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.04),
-                    CustomTaskContainer(
-                      title: "Data Science Quiz",
-                      subtitle: "Date- 19Feb - 28Feb",
-                      icon: Icon(
-                        Icons.quiz,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.04),
-                    CustomTaskContainer(
-                      title: "Data Science Quiz",
-                      subtitle: "Date- 19Feb - 28Feb",
-                      icon: Icon(
-                        Icons.quiz,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.04),
-                    CustomTaskContainer(
-                      title: "Data Science Quiz",
-                      subtitle: "Date- 19Feb - 28Feb",
-                      icon: Icon(
-                        Icons.quiz,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    "Hello, Sakib!",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.01,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Have a nice day.",
+                    style: TextStyle(
+                        color: Colors.black.withOpacity(.5),
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.01,
+              ),
+              Row(
+                children: [
+                  Customcontainer(
+                    title: "My Task",
+                    onTap: () {},
+                  ),
+                  SizedBox(width: MediaQuery.of(context).size.width * 0.04),
+                  Customcontainer(
+                    title: "In-Progress",
+                    onTap: () {},
+                  ),
+                  SizedBox(width: MediaQuery.of(context).size.width * 0.04),
+                  Customcontainer(
+                    title: "Completed",
+                    onTap: () {},
+                  ),
+                ],
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+              Text("Progress",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600)),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+              CustomTaskContainer(
+                title: "Data Science Quiz",
+                subtitle: "Date- 19Feb - 28Feb",
+                icon: Icon(
+                  Icons.quiz,
+                  color: Colors.white,
                 ),
               ),
-            ]),
-          )),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+              CustomTaskContainer(
+                title: "Data Science Presentation",
+                subtitle: "Date- 20Feb - 28Feb",
+                icon: Icon(
+                  Icons.present_to_all,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+              CustomTaskContainer(
+                title: "Data Science Quiz",
+                subtitle: "Date- 19Feb - 28Feb",
+                icon: Icon(
+                  Icons.quiz,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+              CustomTaskContainer(
+                title: "Data Science Quiz",
+                subtitle: "Date- 19Feb - 28Feb",
+                icon: Icon(
+                  Icons.quiz,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+              CustomTaskContainer(
+                title: "Data Science Quiz",
+                subtitle: "Date- 19Feb - 28Feb",
+                icon: Icon(
+                  Icons.quiz,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
